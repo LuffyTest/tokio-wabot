dde27e044bfe7b357ce7c8fc0830ece1395758b2
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-07 20:32:38 +0530

    Update xhamstersearch.js

--- plugins/xhamstersearch.js
+++ plugins/xhamstersearch.js
@@ -1,7 +1,9 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
 case 'xhamstersearch':
    if (args.length == 0) return reply(`Example: ${prefix + command} Japanese`)
    query = args.join(" ")
-   get_result = await fetchJson(`https://api.lolhuman.xyz/api/xhamstersearch?apikey=${apikey}&query=${query}`)
+   get_result = await fetchJson(`https://api.lolhuman.xyz/api/xhamstersearch?apikey=711994c4ea9aa5a0ec39f7f2&query=${query}`)
    get_result = get_result.result
    ini_txt = ""
    for (var x of get_result) {
@@ -9,5 +11,11 @@ case 'xhamstersearch':
        ini_txt += `Views : ${x.views}\n`
        ini_txt += `Duration : ${x.duration}\n`
        ini_txt += `Link : ${x.link}\n\n`
-   }
+     }
    reply(ini_txt)
+}
+handler.help = ['', 'earch'].map(v => 'xhamstersearch' + v + ' <search>')
+handler.tags = ['downloader']
+handler.command = /^xhs(earch)?$/i
+
+module.exports = handler
