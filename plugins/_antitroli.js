b18561ab6c379a51ac53127008c7b1ca4177b260
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-07 18:35:11 +0530

    Update _antitroli.js

--- plugins/_antitroli.js
+++ plugins/_antitroli.js
@@ -1,12 +1,17 @@
 let handler = m => m
 
 handler.all = async function (m) {
+    if (!db.data.settings[this.user.jid].antitroli) return // antitroli aktif?
     if (m.message && m.isBaileys && m.quoted && m.quoted.mtype === 'orderMessage' && !(m.quoted.token && m.quoted.orderId)) {
-        m.reply('Bug Troli Detected\n\n' + require('util').format(m.key))
-        // await this.clearMessage(m.chat, m.key)
+        m.reply('Troli Detected 😒\n\n' + require('util').format(m.key), null)
         await this.modifyChat(m.chat, 'clear', {
             includeStarred: false
         }).catch(console.log)
+        this.reply(owner[0] + '@s.whatsapp.net', `
+🔰 Trolli sender @${m.sender.split`@`[0]}
+🔰 ID: ${m.chat}
+🔰 Name: ${this.getName(m.chat)}
+`.trim(), null, { contextInfo: { mentionedJid: [m.sender] } })
     }
 }
 
