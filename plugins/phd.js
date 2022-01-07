6552ee00f3657e4e32ce18a4bf367cc7d6a2a5fd
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-07 16:05:23 +0530

    Update phd.js

--- plugins/phd.js
+++ plugins/phd.js
@@ -1,26 +1,20 @@
-let fetch = require('node-fetch')
+const fetch = require('node-fetch')
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
-const { servers, ph } = require('../lib/ph')
+const pornhub = require('../lib/ph')
 case 'phdl':
-                if (!isUrl(url) && !url.includes('pornhub.com')) return await hisoka.reply(from, ind.wrongFormat(), id)
-                if (isGroupMsg) {
-                    if (!isNsfw) return await hisoka.reply(from, ind.notNsfw(), id)
-                    if (limit.isLimit(sender.id, _limit, limitCount, isPremium, isOwner)) return await hisoka.reply(from, ind.limit(), id)
-                    limit.addLimit(sender.id, _limit, isPremium, isOwner)
-                    await hisoka.reply(from, ind.wait(), id)
-                    try {
-                        nsfw.phDl(url)
+               
+                        downloader.phDl(url)
                             .then(async ({ title, download_urls, thumbnail_url }) => {
                                 const count = Object.keys(download_urls).length
                                 if (count !== 2) {
                                     const shortsLow = await misc.shortener(download_urls['240P'])
                                     const shortsMid = await misc.shortener(download_urls['480P'])
                                     const shortsHigh = await misc.shortener(download_urls['720P'])
-                                    await hisoka.sendFileFromUrl(from, thumbnail_url, `${title}`, `Title: ${title}\n\nLinks:\n${shortsLow} (240P)\n${shortsMid} (480P)\n${shortsHigh} (720P)`, id)
+                                    await conn.sendFileFromUrl(from, thumbnail_url, `${title}`, `Title: ${title}\n\nLinks:\n${shortsLow} (240P)\n${shortsMid} (480P)\n${shortsHigh} (720P)`, id)
                                         .then(() => console.log('Success sending pornhub metadata!'))
                                 } else {
                                     const shortsLow = await misc.shortener(download_urls['240P'])
-                                    await hisoka.sendFileFromUrl(from, thumbnail_url, `${title}`, `Title: ${title}\n\nLinks:\n${shortsLow} (240P)`, id)
+                                    await conn.sendFileFromUrl(from, thumbnail_url, `${title}`, `Title: ${title}\n\nLinks:\n${shortsLow} (240P)`, id)
                                         .then(() => console.log('Success sending pornhub metadata!'))
                                 }
                             })
@@ -29,9 +23,9 @@ case 'phdl':
                         await hisoka.reply(from, 'Error!', id)
                     }
                 } else {
-                    if (limit.isLimit(sender.id, _limit, limitCount, isPremium, isOwner)) return await hisoka.reply(from, ind.limit(), id)
+                    if (limit.isLimit(sender.id, _limit, limitCount, isPremium, isOwner)) return await conn.reply(from, ind.limit(), id)
                     limit.addLimit(sender.id, _limit, isPremium, isOwner)
-                    await hisoka.reply(from, ind.wait(), id)
+                    await conn.reply(from, ind.wait(), id)
                     try {
                         nsfw.phDl(url)
                             .then(async ({ title, download_urls, thumbnail_url }) => {
@@ -40,17 +34,17 @@ case 'phdl':
                                     const shortsLow = await misc.shortener(download_urls['240P'])
                                     const shortsMid = await misc.shortener(download_urls['480P'])
                                     const shortsHigh = await misc.shortener(download_urls['720P'])
-                                    await hisoka.sendFileFromUrl(from, thumbnail_url, `${title}`, `Title: ${title}\n\nLinks:\n${shortsLow} (240P)\n${shortsMid} (480P)\n${shortsHigh} (720P)`, id)
+                                    await conn.sendFileFromUrl(from, thumbnail_url, `${title}`, `Title: ${title}\n\nLinks:\n${shortsLow} (240P)\n${shortsMid} (480P)\n${shortsHigh} (720P)`, id)
                                         .then(() => console.log('Success sending pornhub metadata!'))
                                 } else {
                                     const shortsLow = await misc.shortener(download_urls['240P'])
-                                    await hisoka.sendFileFromUrl(from, thumbnail_url, `${title}`, `Title: ${title}\n\nLinks:\n${shortsLow} (240P)`, id)
+                                    await conn.sendFileFromUrl(from, thumbnail_url, `${title}`, `Title: ${title}\n\nLinks:\n${shortsLow} (240P)`, id)
                                         .then(() => console.log('Success sending pornhub metadata!'))
                                 }
                             })
                     } catch (err) {
                         console.error(err)
-                        await hisoka.reply(from, 'Error!', id
+                        await conn.reply(from, 'Error!', id
                     }
 handler.help = ['PornHub'].map(v => v + ' <url>')
 handler.tags = ['downloader']
