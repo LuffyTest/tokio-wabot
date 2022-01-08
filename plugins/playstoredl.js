3bf82336956b8ad2c1a49971775db1f4639ca31b
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-08 10:28:06 +0530

    Update playstoredl.js

--- plugins/playstoredl.js
+++ plugins/playstoredl.js
@@ -2,11 +2,16 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `🔺 Usage:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://play.google.com`
-    fetchJson(`https://api.neoxr.eu.org/api/playstore?${url}&apikey=1RB98Fy9gg`)
+    let res = await fetch(global.API('neoxr', '/playstore', {
+    url: args[0]
+  }, 'APIKEY'))
     .then((result) => resolve(result))
     .catch((err) => reject(err))
+  let url = json.server_1 || json.info[0].link || ''
+  if (!url) throw 'Failed to fetch download url'
+  let txt = json.info[0].text
     await m.reply(wait)
-    await conn.sendFile(m.chat, json.apk, '.apk', '© Chitoge', m)
+    conn.sendFile( m.chat, url, 'Chitoge.apk', null, m)
 }
 handler.help = ['apk'].map(v => v + ' <url>')
 handler.tags = ['downloader']
