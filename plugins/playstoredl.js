448bf7ae7f59aa415c7810d33845601da618f9c2
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-08 10:12:08 +0530

    Update playstoredl.js

--- plugins/playstoredl.js
+++ plugins/playstoredl.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-    if (!text) throw `🔺 Usage:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://vt.tiktok.com/ZGJBtcsDq/`
-    let res = await fetchJson(`https://api.neoxr.eu.org/api/playstore?${url}&apikey=1RB98Fy9gg`),
+    if (!text) throw `🔺 Usage:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://play.google.com`
+    let res = await fetchJson(`https://api.neoxr.eu.org/api/playstore?${url}&apikey=1RB98Fy9gg`)
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
