8aa3bbbf133a04c04f54906319716c080342a99c
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-08 09:45:21 +0530

    Create playstoredl.js

---
+++ plugins/playstoredl.js
@@ -0,0 +1,17 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+    if (!text) throw `🔺 Usage:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://vt.tiktok.com/ZGJBtcsDq/`
+    let res = await fetch(API('neoxr', '/api/playstore', { url: args[0] }, '1RB98Fy9gg'))
+    if (!res.ok) throw eror
+    let json = await res.json()
+    if (!json.status) throw json
+    await m.reply(wait)
+    await conn.sendFile(m.chat, json.apk, '.apk', '© Chitoge', m)
+}
+handler.help = ['apk'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(apk)$/i
+
+
+module.exports = handler
