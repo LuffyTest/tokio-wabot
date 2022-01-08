9a607bd335d232f8da68a713e4b9d64888aa4c80
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-08 10:47:24 +0530

    Update playstoredl.js

--- plugins/playstoredl.js
+++ plugins/playstoredl.js
@@ -2,13 +2,11 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `🔺 Usage:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://play.google.com`
-    let res = await fetch(global.API('neoxr', '/playstore', {
-    url: args[0]
-  }, 'APIKEY'))
+    let res = await fetch(global.API('neoxr', '/playstore', {  url: args[0]}, 'APIKEY'))
     .then((result) => resolve(result))
     .catch((err) => reject(err))
-  let url = json.server_1 || json.info[0].link || ''
-  if (!url) throw 'Failed to fetch download url'
+  let url = json.info[0].link ''
+  if (!link) throw 'Failed to fetch download url'
   let txt = json.info[0].text
     await m.reply(wait)
     conn.sendFile( m.chat, url, 'Chitoge.apk', null, m)
