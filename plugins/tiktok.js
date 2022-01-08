9be889b29fb2899cd67fd52a173f70e10802b4d5
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-08 08:43:29 +0530

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `🔺 Usage:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://vt.tiktok.com/ZGJBtcsDq/`
-    let res = await fetch(API('amel', '/tiktok', { url: text }, 'apikey'))
+    let res = await fetch(API('l0lhuman', '/api/download/tiktok', { url: args[0] }, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
