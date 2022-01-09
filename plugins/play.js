4a0f627aa8a9966f5ce31135eff4c518f2abf533
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-09 14:26:12 +0530

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,53 +1,41 @@
-let limit = 30
+const { servers, yta, ytv } = require('../lib/y2mate')
 let yts = require('yt-search')
 let fetch = require('node-fetch')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
-  if (!text) throw 'What are you looking for?'
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `What are you looking for?\n\n🔮 Example :\n${usedPrefix + command} california`
   let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Not found'
+  if (!vid) throw 'Konten Tidak ditemukan'
   let isVideo = /2$/.test(command)
   let yt = false
+  let yt2 = false
   let usedServer = servers[0]
   for (let i in servers) {
     let server = servers[i]
     try {
-      yt = await (isVideo ? ytv : yta)(vid.url, server)
+      yt = await yta(vid.url, server)
+      yt2 = await ytv(vid.url, server)
       usedServer = server
       break
     } catch (e) {
       m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\ntried another server...'}`)
     }
   }
-  if (yt === false) throw 'All servers cannot :/'
+  if (yt === false) throw 'All Servers Down 😥'
+  if (yt2 === false) throw 'All Servers Down 😪'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*💫 Title:* ${title}
-*🔻 Filesize:* ${filesizeF}
-*🎭 Source:* ${vid.url}
- ${isLimit ? 'Used ': ''}
-`.trim(), m)
-let _thumb = {}
-try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-catch (e) { }
-if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*💫 Title:* ${title}
-*🔺 Filesize:* ${filesizeF}
-*🎭 Source:* ${vid.url}
-`.trim(), m, false,  {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+  await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
+*💫 Name:* ${title}
+*▪️Audio File Size:* ${filesizeF}
+*▫️Video File Size:* ${yt2.filesizeF}
+*🔺Server y2mate:* ${usedServer}
+`.trim(), '© Chitoge', '🔸 ᴀᴜᴅɪᴏ 🔸', `.yta ${vid.url}`, '🔹 ᴠɪᴅᴇᴏ 🔹', `.yt ${vid.url}`)
 }
-handler.help = ['play', 'play2'].map(v => v + ' <search>')
+handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
-handler.command = /^play2?$/i
+handler.command = /^(p|play)$/i
 
 handler.exp = 0
-handler.limit = true
 
 module.exports = handler
-
