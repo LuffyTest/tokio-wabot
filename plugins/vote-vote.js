b562be4a61eae3233c73adafec6f1794e4fa64ed
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-10 09:27:19 +0530

    Update vote-vote.js

--- plugins/vote-vote.js
+++ plugins/vote-vote.js
@@ -1,38 +1,35 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
-    if (!(id in conn.vote)) throw `_*no voting in this group!*_\n\n*${usedPrefix}start* - to start voting`
+    if (!(id in conn.vote)) return conn.sendButton(m.chat, `No voting in this group!`, '© Chitoge <3', '▫️ sʀᴀʀᴛ ▫️', `${usedPrefix}+vote`, m)
     let isVote = conn.vote[id][1].concat(conn.vote[id][2])
     const wasVote = isVote.includes(m.sender)
-    if (wasVote) throw 'You\'ve voted!'
+    if (wasVote) throw 'You have voted!'
     if (/up/i.test(command)) {
         conn.vote[id][1].push(m.sender)
     } else if (/de/i.test(command)) {
         conn.vote[id][2].push(m.sender)
     }
-    m.reply(`Done!\n\n*${usedPrefix}checkvote* - to check the vote`)
     let [reason, upvote, devote] = conn.vote[id]
-    let mentionedJid = [...upvote, ...devote]
-    m.reply(`
-*「 VOTE 」*
+    let caption = `
+「 *🔸 Vote 🔸* 」
 
-*Reason:* ${reason}
+*🔹 Reason:* ${reason}
 
-*UPVOTE*
+*🔺 Upvote 🔺*
 _Total: ${upvote.length}_
-${upvote.map(u => '@' + u.split('@')[0]).join('\n')}
+${upvote.map(u => '@' + u.split`@`[0]).join('\n')}
 
-*DEVOTE*
+*🔻 Devote 🔻*
 _Total: ${devote.length}_
-${devote.map(u => '@' + u.split('@')[0]).join('\n')}
+${devote.map(u => '@' + u.split`@`[0]).join('\n')}
 
-*${usedPrefix}delvote* - to delete votes
-
-_by david_
-`.trim(), false, { contextInfo: { mentionedJid } })
+© Chitoge <3
+    `.trim()
+    conn.send2Button(m.chat, caption, '© Chitoge <3', 'Upvote', `${usedPrefix}upvote`, 'Devote', `${usedPrefix}devote`, m)
 }
 handler.help = ['upvote', 'devote']
 handler.tags = ['vote']
 handler.command = /^(up|de)vote$/i
-handler.group = true
+
 module.exports = handler
