0c37e6099b53a76ebd34b09edcc5a209453cc418
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-10 09:34:48 +0530

    Update vote-start.js

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -1,20 +1,23 @@
-let handler = async (m, { conn, text, usedPrefix }) => {
+let handler = async (m, { conn, text, usedPrefix, isAdmin, isOwner }) => {
+    if (m.isGroup) {
+        if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
+    }
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
-    if (id in conn.vote) {
-        throw `_There are still votes in this chat!_\n\n*${usedPrefix}unvote* - to delete votes`
-    }
-    m.reply(`Voting begins!\n\n*${usedPrefix}upvote* - for sure\n*${usedPrefix}devote* - for not\n*${usedPrefix}checkvote* - to check the vote\n*${usedPrefix}delvote* - to delete votes`)
+    if (id in conn.vote) return conn.sendButton(m.chat, 'There are still votes in this chat!', '© Chitoge <3', 'Delete', `${usedPrefix}-vote`, m)
+    conn.send2Button(m.chat, `Vote Started!
+*${usedPrefix}upvote* - To Agree
+*${usedPrefix}devote* - To Disagree
+*${usedPrefix}check Vote* - To Check Votes
+*${usedPrefix}unvote* - To Delete vote`, '© Chitoge <3', '🔺 ᴜᴘᴠᴏᴛᴇ 🔺', `${usedPrefix}upvote`, '🔻 ᴅᴇᴠᴏᴛᴇ 🔻', `${usedPrefix}devote`, m)
     conn.vote[id] = [
         text,
         [],
         []
     ]
 }
-handler.help = ['startvote [reason]']
+handler.help = ['startvote [Reason]']
 handler.tags = ['vote']
-handler.command = /^(start|mulai)vote$/i
-handler.limit = true
-handler.group = true
-handler.admin = true
+handler.command = /^(start|mulai|\+)vote$/i
+
 module.exports = handler
