7d14db56cec2418a1f8c34124831f49db4bb3646
Dularazero <86337319+Dularazero@users.noreply.github.com>
2022-01-20 14:20:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,56 +1,79 @@
 let fs = require('fs')
-let chalk = require('chalk')
 
-global.owner = ['94611392491'] // Put your number here
+global.owner = ['9471392491'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
-global.db = 'mongodb+srv://Akame:Dulara2003@cluster0.s8qad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
-  dzx: 'https://api.dhamzxploit.my.id',
+  bg: 'http://bochil.ddns.net',
   xteam: 'https://api.xteam.xyz',
+  melcanz: 'httpa://melcanz.com',
+  lol: 'https://api.lolhuman.xyz',
   zahir: 'https://zahirr-web.herokuapp.com',
   zeks: 'https://api.zeks.xyz',
-  neoxr: 'https://api.neoxr.eu.org',
+  pencarikode: 'https://pencarikode.xyz',
   LeysCoder: 'https://leyscoders-api.herokuapp.com'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': '284d009ce74863ed',
+  'https://api.xteam.xyz': 'apivproject',
+  'https://melcanz.com': 'elaina',
+  'https://api.lolhuman.xyz': 'pkebgk8248jskrkfm',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.xyz': 'rNOP3N9WFFY7tqbMeNp1lmWkdwLqqqq',
-  'https://api.neoxr.eu.org': '1RB98Fy9gg',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
 // Sticker WM
+global.stiker_wait = 'Wait'
+global.packname = 'ShiraoriBOT Multi device'
+global.author = 'Ilman'
+global.email = 'wakabahiiro5@gmail.com'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.wm = '© Luff X-Md Made by Ilman'
+global.media = 'https://telegra.ph/file/a36cb23c3679ed2d0127e.jpg'
 
-const spack = fs.readFileSync("lib/exif.json")
-const stickerpack = JSON.parse(spack)
-if (stickerpack.spackname == '') {
-  var sticker_name = 'I hope you\'re fine'
-  var sticker_author = 'Zero XD'
-} else {
-  var sticker_name = stickerpack.spackname
-  var sticker_author = stickerpack.sauthor
-}
-
-const file_exif = "lib/exif.json"
-fs.watchFile(file_exif, () => {
-  fs.unwatchFile(file_exif)
-  console.log(chalk.redBright("Update 'exif.json'"))
-  delete require.cache[file_exif]
-  require('./lib/exif.json')
-})
-
-global.packname = sticker_name
-global.author = sticker_author
-
+global.wait = '_*tunggu sedang di proses...*_'
+global.eror = '_*Server Error*_'
 
 global.multiplier = 69 // The higher, The harder levelup
 
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      petFood: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
 
+
+let chalk = require('chalk')
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
   fs.unwatchFile(file)
